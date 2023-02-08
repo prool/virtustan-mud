55c5e7205a8f7ef6ec8f25dc454d994a7eb62c81
Prool <proolix@gmail.com>
2023-02-08 14:59:11 +0200

    Переделал генератор 1. Теперь описания и параметры генерируемой зоны не в header файле, а в конфиге. Таким образом генерация новой зоны или нового варианта старой зоны не требует перекомпиляции. Генератор 2 переделаю по такому же варианту чуть позже.

--- lib/world/obj/1014.obj
+++ lib/world/obj/1014.obj
@@ -1,3 +1,3 @@
-* Created by Zone Generator by proolix@gmail.com Tue Feb  7 16:10:20 2023
+* Created by Zone Generator by proolix@gmail.com Wed Feb  8 14:43:23 2023
 $
 $
--- utilities/generator/1/generator.c
+++ utilities/generator/1/generator.c
@@ -1,11 +1,13 @@
-// zone generator 0.1 by prool@itl.ua
+// zone generator by prool <proolix@gmail.com> http://mud.kharkov.org
+// cyrillic letters in codetable koi8-r
+// кириллица в кодировке koi8-r
+
+#define VERSION "0.2"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-
-#include "config.h"
 
 #define NAMELEN 200
 #define COPYLEFT "Created by Zone Generator by proolix@gmail.com"
@@ -16,8 +18,13 @@
 #define ZAPAD 3
 #define VVERH 4
 #define VNIZ 5
-#define CALC(i,j) j-1+(i-1)*10+ZONE*100
-#define EXIT(DIREKT) fprintf(fw, "D%i\n~\n~\n%i %i %i\n", DIREKT, ROOM_EXIT_FLAG, ROOM_EXIT_KEY, exit_num(DIREKT, i, j));
+#define CALC(i,j) j-1+(i-1)*10+zone_num*100
+#define EXIT(DIREKT) fprintf(fw, "D%i\n~\n~\n%s %s %i\n", DIREKT, ROOM_EXIT_FLAG, ROOM_EXIT_KEY, exit_num(DIREKT, i, j));
+
+#define MAX_STR 512
+#define MAX_PARAM 20
+
+int zone_num;
 
 char *ptime(void);  // Возвращаемое значение: ссылка на текстовую строку с текущим временем
 
@@ -35,7 +42,7 @@ switch (direkt)
 
 int main (void)
 {
-int i, j;
+int i, ii, j, param_num;
 int mob_vnum, room_vnum, max_in_room, max_in_world;
 
 FILE *fz;
@@ -50,12 +57,166 @@ char nameo[NAMELEN];
 char namet[NAMELEN];
 char namew[NAMELEN];
 
+char buf[MAX_STR];
+
+char ZONE [MAX_STR];
+char ZONE_NAME [MAX_STR];
+char REPOP_TIME [MAX_STR];
+char REPOP_TYPE [MAX_STR];
+char MOB_IFFLAG [MAX_STR];
+char ALIAS [MAX_STR];
+char PADEZH1 [MAX_STR];
+char PADEZH2 [MAX_STR];
+char PADEZH3 [MAX_STR];
+char PADEZH4 [MAX_STR];
+char PADEZH5 [MAX_STR];
+char PADEZH6 [MAX_STR];
+char DESCR [MAX_STR];
+char LONG_DESCR [MAX_STR];
+char MOB_FLAG [MAX_STR];
+char MOB_AFF [MAX_STR];
+char ALIGN [MAX_STR];
+char MOB_LVL [MAX_STR];
+char MOB_HITROLL [MAX_STR];
+char MOB_AC [MAX_STR];
+char MOB_HIT [MAX_STR];
+char MOB_DAMAGE [MAX_STR];
+char MOB_MONEY [MAX_STR];
+char MOB_EXPERIENSE [MAX_STR];
+char MOB_POS_BOOT [MAX_STR];
+char MOB_POS_DEFAULT [MAX_STR];
+char MOB_SEX [MAX_STR];
+char ROOM_TITLE [MAX_STR];
+char ROOM_DESCR [MAX_STR];
+char ROOM_FLAGS [MAX_STR];
+char ROOM_SECTOR_TYPE [MAX_STR];
+char ROOM_EXIT_FLAG [MAX_STR];
+char ROOM_EXIT_KEY [MAX_STR];
+
+char MOB_PARAMETERS [MAX_STR][MAX_PARAM];
+
+char *cc;
+FILE *fp;
+
+printf("Zone generator for MUD by Prool %s\n",VERSION);
+
+for (i=0;i<MAX_PARAM;i++) MOB_PARAMETERS[0][i]=0;
+
+ZONE [0]=0;
+ZONE_NAME [0]=0;
+REPOP_TIME [0]=0;
+REPOP_TYPE [0]=0;
+MOB_IFFLAG [0]=0;
+ALIAS [0]=0;
+PADEZH1 [0]=0;
+PADEZH2 [0]=0;
+PADEZH3 [0]=0;
+PADEZH4 [0]=0;
+PADEZH5 [0]=0;
+PADEZH6 [0]=0;
+DESCR [0]=0;
+LONG_DESCR [0]=0;
+MOB_FLAG [0]=0;
+MOB_AFF [0]=0;
+ALIGN [0]=0;
+MOB_LVL [0]=0;
+MOB_HITROLL [0]=0;
+MOB_AC [0]=0;
+MOB_HIT [0]=0;
+MOB_DAMAGE [0]=0;
+MOB_MONEY [0]=0;
+MOB_EXPERIENSE [0]=0;
+MOB_POS_BOOT [0]=0;
+MOB_POS_DEFAULT [0]=0;
+MOB_SEX [0]=0;
+ROOM_TITLE [0]=0;
+ROOM_DESCR [0]=0;
+ROOM_FLAGS [0]=0;
+ROOM_SECTOR_TYPE [0]=0;
+ROOM_EXIT_FLAG [0]=0;
+ROOM_EXIT_KEY [0]=0;
+
+// read config
+fp=fopen("generator.cfg","r");
+if (fp==NULL) {
+printf("File generator.cfg not found\n");
+return -1;
+}
+
+while(1) {
+buf[0]=0;
+fgets(buf, MAX_STR, fp);
+if (buf[0]==0) break;
+cc=strchr(buf,'\r'); if (cc) *cc=0;
+cc=strchr(buf,'\n'); if (cc) *cc=0;
+if (buf[0]==0) continue;
+if (buf[0]=='#') continue;
+cc=strchr(buf,' ');
+if (cc==0) continue;
+*cc=0;
+if (!strcmp(buf,"ZONE")) strcpy(ZONE,cc+1);
+else if (!strcmp(buf,"ZONE_NAME")) strcpy(ZONE_NAME,cc+1);
+else if (!strcmp(buf,"REPOP_TIME")) strcpy(REPOP_TIME,cc+1);
+else if (!strcmp(buf,"REPOP_TYPE")) strcpy(REPOP_TYPE,cc+1);
+else if (!strcmp(buf,"MOB_IFFLAG")) strcpy(MOB_IFFLAG,cc+1);
+else if (!strcmp(buf,"ALIAS")) strcpy(ALIAS,cc+1);
+else if (!strcmp(buf,"PADEZH1")) strcpy(PADEZH1,cc+1);
+else if (!strcmp(buf,"PADEZH2")) strcpy(PADEZH2,cc+1);
+else if (!strcmp(buf,"PADEZH3")) strcpy(PADEZH3,cc+1);
+else if (!strcmp(buf,"PADEZH4")) strcpy(PADEZH4,cc+1);
+else if (!strcmp(buf,"PADEZH5")) strcpy(PADEZH5,cc+1);
+else if (!strcmp(buf,"PADEZH6")) strcpy(PADEZH6,cc+1);
+else if (!strcmp(buf,"DESCR")) strcpy(DESCR,cc+1);
+else if (!strcmp(buf,"LONG_DESCR")) strcpy(LONG_DESCR,cc+1);
+else if (!strcmp(buf,"MOB_FLAG")) strcpy(MOB_FLAG,cc+1);
+else if (!strcmp(buf,"MOB_AFF")) strcpy(MOB_AFF,cc+1);
+else if (!strcmp(buf,"ALIGN")) strcpy(ALIGN,cc+1);
+else if (!strcmp(buf,"MOB_LVL")) strcpy(MOB_LVL,cc+1);
+else if (!strcmp(buf,"MOB_HITROLL")) strcpy(MOB_HITROLL,cc+1);
+else if (!strcmp(buf,"MOB_AC")) strcpy(MOB_AC,cc+1);
+else if (!strcmp(buf,"MOB_HIT")) strcpy(MOB_HIT,cc+1);
+else if (!strcmp(buf,"MOB_DAMAGE")) strcpy(MOB_DAMAGE,cc+1);
+else if (!strcmp(buf,"MOB_MONEY")) strcpy(MOB_MONEY,cc+1);
+else if (!strcmp(buf,"MOB_EXPERIENSE")) strcpy(MOB_EXPERIENSE,cc+1);
+else if (!strcmp(buf,"MOB_POS_BOOT")) strcpy(MOB_POS_BOOT,cc+1);
+else if (!strcmp(buf,"MOB_POS_DEFAULT")) strcpy(MOB_POS_DEFAULT,cc+1);
+else if (!strcmp(buf,"MOB_SEX")) strcpy(MOB_SEX,cc+1);
+else if (!strcmp(buf,"ROOM_TITLE")) strcpy(ROOM_TITLE,cc+1);
+else if (!strcmp(buf,"ROOM_DESCR")) strcpy(ROOM_DESCR,cc+1);
+else if (!strcmp(buf,"ROOM_FLAGS")) strcpy(ROOM_FLAGS,cc+1);
+else if (!strcmp(buf,"ROOM_SECTOR_TYPE")) strcpy(ROOM_SECTOR_TYPE,cc+1);
+else if (!strcmp(buf,"ROOM_EXIT_FLAG")) strcpy(ROOM_EXIT_FLAG,cc+1);
+else if (!strcmp(buf,"ROOM_EXIT_KEY")) strcpy(ROOM_EXIT_KEY,cc+1);
+else if (!strcmp(buf,"MOB_PARAMETERS")) {
+	if (param_num>=MAX_PARAM) {
+		printf("Too many MOB_PARAMETERS\n");
+		continue;
+		}
+	else	{
+		for(i=0;i<=MAX_STR;i++) {
+			MOB_PARAMETERS[i][param_num]=*(cc+1+i);
+			if (*(cc+1+i)==0) break;
+			}
+		param_num++;
+		}
+	}
+else printf("debug: read '%s'\n", buf);
+}
+
+fclose(fp);
+
+zone_num=atoi(ZONE);
+if (zone_num<=0) {
+printf("zone_num error. zone_num=%i\n", zone_num);
+return -1;
+}
+
 // генерация имен файлов
-sprintf(namez,"%i.zon",ZONE);
-sprintf(namem,"%i.mob",ZONE);
-sprintf(nameo,"%i.obj",ZONE);
-sprintf(namet,"%i.trg",ZONE);
-sprintf(namew,"%i.wld",ZONE);
+sprintf(namez,"%i.zon",zone_num);
+sprintf(namem,"%i.mob",zone_num);
+sprintf(nameo,"%i.obj",zone_num);
+sprintf(namet,"%i.trg",zone_num);
+sprintf(namew,"%i.wld",zone_num);
 
 if ((fz=fopen (namez, "w"))==NULL) exit(1);
 if ((fm=fopen (namem, "w"))==NULL) exit(1);
@@ -74,10 +235,10 @@ fputs("$\n$\n", fo);
 // формирование zon файла
 
 fprintf(fz,"* %s %s\n", COPYLEFT, ptime());
-fprintf(fz,"#%i\n",ZONE);
+fprintf(fz,"#%i\n",zone_num);
 fprintf(fz,"%s~\n",ZONE_NAME);
 fprintf(fz,"#1 0 0\n"); // for Zerkalo: эта строка нужна для Зеркала. код Виртустана работает и с ней и без неё
-fprintf(fz,"%i99 %i %i\n",ZONE,REPOP_TIME,REPOP_TYPE);
+fprintf(fz,"%i99 %s %s\n",zone_num,REPOP_TIME,REPOP_TYPE);
 
 // mobs to zonefile
 for (i=0;i<90;i++)
@@ -87,8 +248,8 @@ for (i=0;i<90;i++)
     room_vnum=i;
     max_in_world=1;
     fprintf(fz,"M %s %i%02i %i %i%02i %i (mob number %i)\n",MOB_IFFLAG,
-    ZONE, mob_vnum, max_in_room,
-    ZONE, room_vnum, max_in_world, mob_vnum);
+    zone_num, mob_vnum, max_in_room,
+    zone_num, room_vnum, max_in_world, mob_vnum);
     }
 
 fputs("S\n$\n$\n",fz);
@@ -100,7 +261,7 @@ fprintf(fm, "* %s %s\n", COPYLEFT, ptime());
 for (i=0;i<90;i++)
 { // генерация зверя
 mob_vnum=i;
-fprintf(fm, "#%i%02i\n", ZONE, mob_vnum);
+fprintf(fm, "#%i%02i\n", zone_num, mob_vnum);
 fprintf(fm, "%s~\n", ALIAS);
 
 fprintf(fm, "%s %i~\n", PADEZH1,i);
@@ -115,13 +276,23 @@ fputs("~\n", fm);
 
 fprintf(fm, "%s\n", LONG_DESCR);
 fputs("~\n", fm);
-fprintf(fm, "%s %s %i E\n", MOB_FLAG, MOB_AFF, ALIGN);
-fprintf(fm, "%i %i %i %s %s\n", MOB_LVL, MOB_HITROLL, MOB_AC, MOB_HIT,
+fprintf(fm, "%s %s %s E\n", MOB_FLAG, MOB_AFF, ALIGN);
+fprintf(fm, "%s %s %s %s %s\n", MOB_LVL, MOB_HITROLL, MOB_AC, MOB_HIT,
 MOB_DAMAGE);
-fprintf(fm, "%s %i\n", MOB_MONEY, MOB_EXPERIENSE);
-fprintf(fm, "%i %i %i\n", MOB_POS_BOOT, MOB_POS_DEFAULT, MOB_SEX);
+fprintf(fm, "%s %s\n", MOB_MONEY, MOB_EXPERIENSE);
+fprintf(fm, "%s %s %s\n", MOB_POS_BOOT, MOB_POS_DEFAULT, MOB_SEX);
 
-fprintf(fm, "%s\n", MOB_PARAMETERS);
+//fprintf(fm, "%s\n", MOB_PARAMETERS):
+
+#if 1
+for (j=0;j<param_num;j++) {
+		for(ii=0;ii<=MAX_STR;ii++) {
+			if (MOB_PARAMETERS[ii][j]==0) break;
+			fprintf(fm,"%c",MOB_PARAMETERS[ii][j]);
+			}
+		fprintf(fm,"\n");
+}
+#endif
 
 fputs("E\n",fm);
 } // генерация зверя end
@@ -129,7 +300,7 @@ fputs("E\n",fm);
 fputs("$\n$\n",fm);
 
 // формирование файла wld
-fprintf(fw,"* %s\n", COPYLEFT);
+fprintf(fw,"* %s %s\n", COPYLEFT, ptime());
 
 for (i=1;i<=9; i++)
 	for (j=1;j<=10; j++)
@@ -139,7 +310,7 @@ for (i=1;i<=9; i++)
 		fprintf(fw, "%s (%i,%i)~\n", ROOM_TITLE, i, j);
 		fprintf(fw,"   %s\n", ROOM_DESCR);
 		fputs("~\n",fw);
-		fprintf(fw, "%i %i %i\n", ZONE, ROOM_FLAGS, ROOM_SECTOR_TYPE);
+		fprintf(fw, "%i %s %s\n", zone_num, ROOM_FLAGS, ROOM_SECTOR_TYPE);
 		// выходы
 		// D0 - D5 = север восток юг запад вверх вниз
 		//           D0    D1     D2 D3    D4    D5
